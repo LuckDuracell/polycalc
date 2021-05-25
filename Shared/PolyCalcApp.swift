1132429a077b7c1e57a7fccf4374a904ee937067
Luck <lukedru@yahoo.com>
2021-05-25 15:28:46 -0700

    Initial Commit

---
+++ Shared/PolyCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolyCalcApp.swift
+//  Shared
+//
+//  Created by Luke Drushell on 5/25/21.
+//
+
+import SwiftUI
+
+@main
+struct PolyCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
